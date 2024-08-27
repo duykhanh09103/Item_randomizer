dfbaaeca52fba3c65e4834a936ce4c1f425a59cb
duykhanh09103 <duykhanh09103@gmail.com>
2024-08-27 20:04:08 +0700

    Fix a lot of bug !

--- src/main/java/github/duykhanh09103/randomizer_command.java
+++ src/main/java/github/duykhanh09103/randomizer_command.java
@@ -21,7 +21,7 @@ public class randomizer_command implements CommandExecutor {
 
     private final item_randomizer plugin ;
     private boolean isTaskRunning = false;
-
+    BossBar bossBar = Bukkit.createBossBar("RandItem", BarColor.YELLOW, BarStyle.SEGMENTED_10);
     public randomizer_command(item_randomizer plugin) {
         this.plugin = plugin;
     }
@@ -32,15 +32,16 @@ public class randomizer_command implements CommandExecutor {
      if(sender instanceof Player) {
          Player player = (Player) sender;
          String[] allCommand = {"start", "stop", "setTimer", "help"};
-         if (Arrays.stream(allCommand).noneMatch(label::contains)) {
+         Bukkit.getLogger().info("command:" +command +" label: "+label + " args:" + Arrays.toString(args));
+         if (args.length == 0 ||Arrays.stream(allCommand).noneMatch(args[0]::contains)) {
              player.sendMessage(ChatColor.YELLOW + "ItemRand" + ChatColor.WHITE + " :" + " Wrong usage! use /ItemRand help to show list of usage!");
              return true;
          };
-         if(label.equalsIgnoreCase("help")){
+         if(args[0].equalsIgnoreCase("help")){
              player.sendMessage(ChatColor.YELLOW + "ItemRand" + ChatColor.WHITE +" :"+"This plugin have 4 command in total!"+"\n"+"/ItemRand help : to display this message!"+"\n"+"/ItemRand start : to start the timer and bossbar for randomize items!"+"\n"+"/ItemRand stop : to stop ofc bro what do you expect?"+"\n"+"/ItemRand setTimer <time>: to set the time for how long (in seconds) until the next item ! Default is 10s");
          return true;
          }
-         if(label.equalsIgnoreCase("start")){
+         if(args[0].equalsIgnoreCase("start")){
              int timer = (int) plugin.config.get("Timer");
              if(timer<1){
                  player.sendMessage(ChatColor.YELLOW + "ItemRand" + ChatColor.WHITE +" :"+ChatColor.RED+"Error: The timer cant be lower than 1! current timer: "+timer);
@@ -51,9 +52,9 @@ public class randomizer_command implements CommandExecutor {
                  player.sendMessage(ChatColor.YELLOW + "ItemRand" + ChatColor.WHITE +" :"+ChatColor.RED+"Error: The items randomizer is already running! please stop it first");
              return true;
              }
-             isTaskRunning=true;
-             BossBar bossBar = Bukkit.createBossBar("RandItem", BarColor.YELLOW, BarStyle.SEGMENTED_10);
              bossBar.setProgress(1.0);
+             isTaskRunning=true;
+
              for(Player allplayer:Bukkit.getOnlinePlayers()){
                  bossBar.addPlayer(allplayer);
              }
@@ -72,39 +73,44 @@ public class randomizer_command implements CommandExecutor {
                                   player.getInventory().addItem(randomItem);
                                   player.sendMessage(ChatColor.YELLOW + "ItemRand" + ChatColor.WHITE + " : You have received " + new TranslatableComponent(randomItem.getTranslationKey()).toPlainText());
                               }
+                              countdown = timer;
                           }
                  }
              }.runTaskTimer(plugin, 0, 20L);
            return true;
          }
-         if(label.equalsIgnoreCase("stop")){
+         if(args[0].equalsIgnoreCase("stop")){
              if(!isTaskRunning){
                  player.sendMessage(ChatColor.YELLOW + "ItemRand" + ChatColor.WHITE +" :"+ChatColor.RED+"Error: Nothing running right now! ");
              return true;
              }
              Bukkit.getScheduler().cancelTasks(plugin);
+             bossBar.removeAll();
              player.sendMessage(ChatColor.YELLOW + "ItemRand" + ChatColor.WHITE + " :"+ChatColor.GREEN+" Successfully stop all task!");
              isTaskRunning=false;
              return true;
          }
-         if(label.equalsIgnoreCase("setTimer")){
-             CharSequence cs = Arrays.toString(args);
-             int Timer = Integer.parseInt(Arrays.toString(args));
-             if(args==null) {
-                 player.sendMessage(ChatColor.YELLOW + "ItemRand" + ChatColor.WHITE + " :" + ChatColor.RED + "Error: No argument found! ");
+         if(args[0].equalsIgnoreCase("setTimer")){
+             if (args.length < 2) {
+                 player.sendMessage(ChatColor.YELLOW + "ItemRand" + ChatColor.WHITE + " :" + ChatColor.RED + " Error: No time value provided!");
                  return true;
              }
-             if(!StringUtils.isNumeric(cs)){
-                 player.sendMessage(ChatColor.YELLOW + "ItemRand" + ChatColor.WHITE + " :" + ChatColor.RED + "Error: The argument you provided is not a string ! ");
+
+             String timeStr = args[1];
+             if (!StringUtils.isNumeric(timeStr)) {
+                 player.sendMessage(ChatColor.YELLOW + "ItemRand" + ChatColor.WHITE + " :" + ChatColor.RED + " Error: The time must be a number!");
                  return true;
              }
-             if(Timer<1){
-                 player.sendMessage(ChatColor.YELLOW + "ItemRand" + ChatColor.WHITE + " :" + ChatColor.RED + "Error: The time has to be higher than 1 !You provided: "+Timer);
+
+             int newTimer = Integer.parseInt(timeStr);
+             if (newTimer < 1) {
+                 player.sendMessage(ChatColor.YELLOW + "ItemRand" + ChatColor.WHITE + " :" + ChatColor.RED + " Error: The time must be greater than 0! You provided: " + newTimer);
                  return true;
              }
-             FileConfiguration config = plugin.getConfig();
-             config.set("setTimer",Timer);
-             player.sendMessage(ChatColor.YELLOW + "ItemRand" + ChatColor.WHITE + " :"+ChatColor.GREEN+" Successfully set timer to "+ChatColor.WHITE+Timer+"s");
+
+             plugin.getConfig().set("Timer", newTimer);
+             plugin.saveConfig();
+             player.sendMessage(ChatColor.YELLOW + "ItemRand" + ChatColor.WHITE + " :" + ChatColor.GREEN + " Successfully set the timer to " + ChatColor.WHITE + newTimer + " seconds!");
          }
 
      }
